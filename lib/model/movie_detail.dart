d91ec295606bb5b8e7a4cc5c6de610ccec7b6931
phillzou <phillzou@gmail.com>
2019-03-26 14:37:25 +0800

    add some attributes

--- lib/model/movie_detail.dart
+++ lib/model/movie_detail.dart
@@ -8,13 +8,16 @@ import 'movie_photo.dart';
 class MovieDetail {
 
   MovieRate rating;   // 电影评分
+  String originalTitle; // 原标题
   MovieImage images;  // 电影封面
-  String year;        // 上映时间
+  String year;        // 上映年份
   List<MovieComment> comments; // 评论
   String alt;         // 豆瓣网址
   String id;          // 电影 id
   String title;       // 电影标题
+  List pubdates;      // 上映日期
   List tags;  // 电影标签
+  List durations;
   List genres;  // 电影类型
   List trailerUrls;  // 预告片 url
   List<MovieActor> casts;    // 演员
@@ -23,21 +26,25 @@ class MovieDetail {
   List<MoviePhoto> photos;    // 剧照
   String summary;     // 电影简介
 
-  MovieDetail(this.rating, this.images, this.year, this.comments, this.alt, this.id,
-     this.title, this.tags, this.genres, this.trailerUrls, this.casts, this.directors,
-     this.countries, this.photos);
+  MovieDetail(this.rating, this.originalTitle, this.images, this.year, this.comments, this.alt, this.id,
+     this.title, this.pubdates, this.tags, this.durations, this.genres, this.trailerUrls, this.casts, this.directors,
+     this.countries, this.photos, this.summary);
 
   MovieDetail.fromJson(Map data) {
     rating = MovieRate.fromJson(data['rating']);
+    originalTitle = data['original_title'];
     images = MovieImage.fromJson(data['images']);
     year = data['year'];
     alt = data['alt'];
     id = data['id'];
     title = data['title'];
+    pubdates = data['pubdates']?.cast<String>()?.toList();
     tags = data['tags']?.cast<String>()?.toList();
+    durations = data['durations']?.cast<String>()?.toList();
     genres = data['genres']?.cast<String>()?.toList();
     trailerUrls = data['trailer_urls']?.cast<String>()?.toList();
     countries = data['countries']?.cast<String>()?.toList();
+    summary = data['summary'];
     
     List<MovieComment> commentData = [];
     List<MovieActor> castsData = [];
