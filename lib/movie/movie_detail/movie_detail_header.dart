62ae8613581a3de4c314c03f89af00a687916bd2
phillzou <phillzou@gmail.com>
2019-03-26 14:39:59 +0800

    update movie detail header

--- lib/movie/movie_detail/movie_detail_header.dart
+++ lib/movie/movie_detail/movie_detail_header.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:movie_recommend/public.dart';
 
-import 'dart:ui' as ui;
+
+
+import 'package:movie_recommend/public.dart';
 
 class MovieDetailHeader extends StatelessWidget {
   
-  final MovieDetail moviedDetail;
+  final MovieDetail movieDetail;
   final Color coverColor;
 
-  const MovieDetailHeader(this.moviedDetail, this.coverColor);
+  const MovieDetailHeader(this.movieDetail, this.coverColor);
   
   @override
   Widget build(BuildContext context) {
@@ -22,13 +23,13 @@ class MovieDetailHeader extends StatelessWidget {
       child: Stack(
         children: <Widget>[
           Image(
-            image: CachedNetworkImageProvider(moviedDetail.photos[0].image),
+            image: CachedNetworkImageProvider(movieDetail.photos[0].image),
             fit: BoxFit.cover,
             width: width,
             height: height,
           ),
           Opacity(
-            opacity: 0.8,
+            opacity: 0.7,
             child: Container(color: coverColor, width: width, height: height),
           ),
           buildContent(context),
@@ -46,8 +47,93 @@ class MovieDetailHeader extends StatelessWidget {
       padding: EdgeInsets.fromLTRB(15, 54 + Screen.topSafeHeight, 10, 0),
       color: Colors.transparent,
       child: Row(
-        
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              boxShadow: [
+                new BoxShadow(
+                  color: Color(0x66000000),
+                  offset: new Offset(1.0, 1.0),
+                  blurRadius: 5.0,
+                  spreadRadius: 2
+                )
+              ],
+            ),
+            child: MovieCoverImage(movieDetail.images.large, width: 100, height: 133),
+          ),
+          SizedBox(width: 15,),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(movieDetail.title, 
+                  style: TextStyle(
+                    fontSize: fixedFontSize(20), 
+                    color: AppColor.white, 
+                    fontWeight: FontWeight.bold),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,  
+                ),
+                SizedBox(height: 5,),
+                Text(movieDetail.originalTitle+'（${movieDetail.year}）',
+                      style: TextStyle(
+                        fontSize: fixedFontSize(16),
+                        color: AppColor.white,
+                        fontWeight: FontWeight.bold
+
+                      ),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                ),
+                SizedBox(height: 5,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    new StaticRatingBar(size: 13.0,rate: movieDetail.rating.average/2,),
+                    SizedBox(width: 5,),  
+                    Text(movieDetail.rating.average.toString(),style: TextStyle(color: AppColor.white, fontSize: fixedFontSize(12)),)
+                  ],
+                ),
+                SizedBox(height: 10,),
+                Text('${countries2String(movieDetail.countries)}/${list2String(movieDetail.genres)}/ 上映时间：${list2String(movieDetail.pubdates)}/ 片长：${list2String(movieDetail.durations)}/${actor2String(movieDetail.directors)}/${actor2String(movieDetail.casts)}',
+                  style: TextStyle(
+                    color: AppColor.white,
+                    fontSize: fixedFontSize(12)
+                  ),
+                  maxLines: 3,
+                  overflow: TextOverflow.ellipsis,
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  String actor2String(List<MovieActor> actors) {
+    StringBuffer sb = new StringBuffer();
+    actors.forEach((actor) {
+      sb.write(' ${actor.name} ');
+    });
+    return sb.toString();
+  }
+
+  String list2String(List list) {
+    StringBuffer sb = new StringBuffer();
+    list.forEach((item) {
+      sb.write(' $item ');
+    });
+    return sb.toString();
+  }
+
+  String countries2String(List countries) {
+    StringBuffer sb = new StringBuffer();
+    countries.forEach((country) {
+      sb.write('$country ');
+    });
+    return sb.toString();
   }
 }
