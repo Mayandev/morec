ef34ab028e9f2d1e81522056085b40754ae00342
phillzou <phillzou@gmail.com>
2019-03-27 19:47:56 +0800

    add movie play page

---
+++ lib/movie/movie_detail/movie_video_play.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:chewie/chewie.dart';
+import 'package:video_player/video_player.dart';
+
+import 'package:movie_recommend/public.dart';
+
+class MovieVideoPlay extends StatefulWidget {
+  final String url;
+
+  const MovieVideoPlay({Key key, this.url}) : super(key: key);
+  @override
+  _MovieVideoPlayState createState() => _MovieVideoPlayState();
+}
+
+class _MovieVideoPlayState extends State<MovieVideoPlay> {
+
+  VideoPlayerController videoPlayerController;
+  ChewieController chewieController;
+
+  @override
+  void initState() { 
+    super.initState();
+    videoPlayerController = VideoPlayerController.network(this.widget.url);
+    chewieController = ChewieController(
+      videoPlayerController: videoPlayerController,
+      aspectRatio: 3 / 2,
+      autoPlay: true,
+      looping: true,
+      // Try playing around with some of these other options:
+
+      // showControls: false,
+      // materialProgressColors: ChewieProgressColors(
+      //   playedColor: Colors.red,
+      //   handleColor: Colors.blue,
+      //   backgroundColor: Colors.grey,
+      //   bufferedColor: Colors.lightGreen,
+      // ),
+      // placeholder: Container(
+      //   color: Colors.grey,
+      // ),
+      // autoInitialize: true,
+    );
+  }
+
+  @override
+  void dispose() {
+    videoPlayerController.dispose();
+    chewieController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            color: Colors.black
+        ),
+        constraints: BoxConstraints.expand(
+            height: MediaQuery.of(context).size.height,
+          ),
+        child: Chewie(
+          controller: chewieController
+        )
+      ),
+    );
+  }
+}
